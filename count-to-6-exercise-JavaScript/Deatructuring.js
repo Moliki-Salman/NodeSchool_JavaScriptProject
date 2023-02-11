816aa2b3cb11018f6e25a44ae88a92e1373d8327
Moliki-Salman <salmanameenat@gmail.com>
2023-02-11 16:47:50 +0100

    ahead

---
+++ count-to-6-exercise-JavaScript/Deatructuring.js
@@ -0,0 +1,9 @@
+let userInfo = {
+        userId: "01",
+        username: "meen",
+        email:"sa@hmail",
+        firstName: "Dan",
+        lastName: "Man",
+} 
+let {uername: newUserName, email} = userInfo;
+console.log(newUserName, email)

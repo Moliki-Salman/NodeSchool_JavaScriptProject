f97825270382dd59db6a019ecd35295cb519b4bc
Moliki-Salman <salmanameenat@gmail.com>
2023-02-11 19:18:03 +0100

    if statement exercise

---
+++ Javascripting-nodeSchool/9-if-statement.js
@@ -0,0 +1,6 @@
+var fruit = "orange";
+if (fruit >  fruit.length) {
+	console.log('The fruit name has more than five characters.')
+} else {
+	console.log('The fruit name has five characters or less.')
+}

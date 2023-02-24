96e42fbfb8e4794d32dce6e08f9d486282d481f3
Moliki-Salman <salmanameenat@gmail.com>
2023-02-24 05:19:21 +0100

    corrected arrow function task

--- count-to-6-exercise-JavaScript/2-Template_strings.js
+++ count-to-6-exercise-JavaScript/2-Template_strings.js
@@ -1,4 +1,4 @@
 const sampleName = process.argv[2]
 
 console.log(`Hello, ${sampleName}!
-Your name lowercased is "${sampleName.toLowerCase()}"`);
+Your name lowercased is "${sampleName.toLowerCase()}".`);
--- count-to-6-exercise-JavaScript/3-arrow_functions.js
+++ count-to-6-exercise-JavaScript/3-arrow_functions.js
@@ -1,3 +1,12 @@
 var fruit = process.argv.slice(2); 
-newFruit = fruit.map(string => string[0]);
-console.log(newFruit.join(''));
+var newFruit = fruit.map(string => string[0]);
+console.log(`[${process.argv.slice(2)}] becomes "${newFruit.join('')}"`);
+
+/* official solution
+  var inputs = process.argv.slice(2);
+    var result = inputs.map(s => s[0])
+                       .reduce((soFar, s) => soFar + s);
+    
+    console.log(`[${inputs}] becomes "${result}"`);
+    
+*/
